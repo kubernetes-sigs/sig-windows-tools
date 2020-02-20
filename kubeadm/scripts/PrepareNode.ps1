edbbfa0d50d7c811947f226e39db95f5307b74b6
Gab Satchi <gsatchithanantham@pivotal.io>
2020-02-20 15:48:02 -0500

    Use nssm, support specifying k8s version

--- kubeadm/scripts/PrepareNode.ps1
+++ kubeadm/scripts/PrepareNode.ps1
@@ -1,3 +1,41 @@
+<#
+.SYNOPSIS
+Assists with preparing a Windows VM prior to calling kubeadm join
+
+.DESCRIPTION
+This script assists with joining a Windows node to a cluster.
+- Downloads Kubernetes binaries (kubelet, kubeadm) at the version specified
+- Registers wins as a service in order to run kube-proxy and cni as DaemonSets. More info here: 
+- Registers kubelet as an nssm service. More info on nssm: https://nssm.cc/
+
+.PARAMETER KubernetesVersion
+Kubernetes version to download and use
+
+.EXAMPLE
+PS> .\PrepareNode.ps1 -KubernetesVersion v1.17.0
+
+#>
+
+Param(
+    [parameter(Mandatory = $true, HelpMessage="Kubernetes version to use")]
+    [string] $KubernetesVersion
+)
+$ErrorActionPreference = 'Stop'
+
+function DownloadFile($destination, $source) {
+    Write-Host("Downloading $source to $destination")
+    curl.exe --fail -Lo $destination $source
+
+    if (!$?) {
+        Write-Error "Download $source failed"
+        exit 1
+    }
+}
+
+if (!$KubernetesVersion.StartsWith("v")) {
+    $KubernetesVersion = "v" + $KubernetesVersion
+}
+Write-Host "Using Kubernete version: $KubernetesVersion"
 $global:Powershell = (Get-Command powershell).Source
 $global:PowershellArgs = "-ExecutionPolicy Bypass -NoProfile"
 $global:KubernetesPath = "$env:SystemDrive\k"
@@ -8,13 +46,15 @@ mkdir -force "$global:KubernetesPath"
 $env:Path += ";$global:KubernetesPath"
 [Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
 
-curl.exe -Lo $kubeletBinPath https://dl.k8s.io/v1.17.0/bin/windows/amd64/kubelet.exe
-curl.exe -Lo "$global:KubernetesPath\kubeadm.exe" https://dl.k8s.io/v1.17.0/bin/windows/amd64/kubeadm.exe
-curl.exe -Lo "$global:KubernetesPath\wins.exe" https://github.com/rancher/wins/releases/download/v0.0.4/wins.exe
+DownloadFile $kubeletBinPath https://dl.k8s.io/$KubernetesVersion/bin/windows/amd64/kubelet.exe
+DownloadFile "$global:KubernetesPath\kubeadm.exe" https://dl.k8s.io/$KubernetesVersion/bin/windows/amd64/kubeadm.exe
+DownloadFile "$global:KubernetesPath\wins.exe" https://github.com/rancher/wins/releases/download/v0.0.4/wins.exe
 
 #Create host network to allow kubelet to schedule hostNetwork pods
+Write-Host "Creating Docker host network"
 docker network create -d nat host
 
+Write-Host "Registering wins service"
 wins.exe srv app run --register
 start-service rancher-wins
 
@@ -24,6 +64,12 @@ mkdir -force C:\etc\kubernetes\pki
 New-Item -path C:\var\lib\kubelet\etc\kubernetes\pki -type SymbolicLink -value C:\etc\kubernetes\pki\
 
 cp $PSScriptRoot/StartKubelet.ps1 $global:StartKubeletScript
+Write-Host "Registering kubelet service"
+Get-Command nssm
+if (!$?) {
+    Write-Error "NSSM is not installed."
+    exit 1
+}
 nssm install kubelet $global:Powershell $global:PowershellArgs $global:StartKubeletScript
 nssm set kubelet DependOnService docker
 
