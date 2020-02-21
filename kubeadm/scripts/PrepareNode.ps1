4850bb5d2a2a1c6169b5dbbc9b3f5a1d42eb90e4
Gab Satchi <gsatchithanantham@pivotal.io>
2020-02-21 10:22:44 -0500

    Start kubelet script now generated in PrepareNode

--- kubeadm/scripts/PrepareNode.ps1
+++ kubeadm/scripts/PrepareNode.ps1
@@ -63,7 +63,14 @@ mkdir -force C:\var\lib\kubelet\etc\kubernetes
 mkdir -force C:\etc\kubernetes\pki
 New-Item -path C:\var\lib\kubelet\etc\kubernetes\pki -type SymbolicLink -value C:\etc\kubernetes\pki\
 
-cp $PSScriptRoot/StartKubelet.ps1 $global:StartKubeletScript
+$StartKubeletFileContent = '$FileContent = Get-Content -Path "/var/lib/kubelet/kubeadm-flags.env"
+$global:KubeletArgs = $FileContent.Trim("KUBELET_KUBEADM_ARGS=`"")
+
+$cmd = "C:\k\kubelet.exe --cert-dir=$env:SYSTEMDRIVE\var\lib\kubelet\pki --config=/var/lib/kubelet/config.yaml --bootstrap-kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf --kubeconfig=/etc/kubernetes/kubelet.conf --hostname-override=$(hostname) --pod-infra-container-image=`"mcr.microsoft.com/k8s/core/pause:1.2.0`" --enable-debugging-handlers --cgroups-per-qos=false --enforce-node-allocatable=`"`" --network-plugin=cni --resolv-conf=`"`" --log-dir=/var/log/kubelet --logtostderr=false --image-pull-progress-deadline=20m $global:KubeletArgs"
+
+Invoke-Expression $cmd'
+Set-Content -Path $global:StartKubeletScript -Value $StartKubeletFileContent
+
 Write-Host "Registering kubelet service"
 Get-Command nssm
 if (!$?) {
--- kubeadm/scripts/StartKubelet.ps1
+++
@@ -1,6 +0,0 @@
-$FileContent = Get-Content -Path "/var/lib/kubelet/kubeadm-flags.env"
-$global:KubeletArgs = $FileContent.Trim('KUBELET_KUBEADM_ARGS="')
-
-$cmd = "C:\k\kubelet.exe --cert-dir=$env:SYSTEMDRIVE\var\lib\kubelet\pki --config=/var/lib/kubelet/config.yaml --bootstrap-kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf --kubeconfig=/etc/kubernetes/kubelet.conf --hostname-override=$(hostname) --pod-infra-container-image=`"mcr.microsoft.com/k8s/core/pause:1.2.0`" --enable-debugging-handlers --cgroups-per-qos=false --enforce-node-allocatable=`"`" --network-plugin=cni --resolv-conf=`"`" --log-dir=/var/log/kubelet --logtostderr=false --image-pull-progress-deadline=20m $global:KubeletArgs"
-
-Invoke-Expression $cmd
