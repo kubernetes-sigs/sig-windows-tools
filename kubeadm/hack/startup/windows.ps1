2902bd790832dade450b16432ca85122b95b3199
Ben Moss <ben@mossity.com>
2020-03-19 17:44:40 -0400

    Whitelist netcat in e2e tests

--- kubeadm/hack/startup/windows.ps1
+++ kubeadm/hack/startup/windows.ps1
@@ -2,6 +2,10 @@
 & sc.exe config wuauserv start=disabled
 & sc.exe stop wuauserv
 
+# tell windows defender to allow netcat, which is needed by the e2e tests
+# https://www.microsoft.com/en-us/wdsi/threats/malware-encyclopedia-description?Name=HackTool:Win32/NetCat&ThreatID=2147593673
+Add-MpPreference -ThreadIDDefaultAction_Ids 2147593673 -ThreadIDDefaultAction_Actions 6
+
 curl.exe -sLO https://github.com/kubernetes-sigs/sig-windows-tools/releases/latest/download/PrepareNode.ps1
 .\PrepareNode.ps1 -KubernetesVersion VERSION
 
