d89c36683a6f5b0fe8ecaab21b8b5842f04fdfc3
root <lijunye@q1.com>
2020-04-29 11:00:10 +0800

    Fix source Vip endpoint creation failure

---
+++ kubeadm/kube-proxy/windowsservercore-1809/hns.psm1
@@ -0,0 +1,36 @@
+Param(
+    [parameter(Mandatory = $true)] $NetworkName="flannel.4096"
+)
+
+# get surce vip
+function Get-SourceVip()
+{
+    $hnsNetwork = Get-HnsNetwork | ? Name -EQ $NetworkName.ToLower()
+    $subnet = $hnsNetwork.Subnets[0].AddressPrefix
+
+    $ipamConfig = @"
+{"cniVersion": "0.2.0", "name": "$NetworkName", "ipam":{"type":"host-local","ranges":[[{"subnet":"$subnet"}]],"dataDir":"/var/lib/cni/networks"}}
+"@
+
+    $ipamConfig | Out-File "c:\k\sourceVipRequest.json"
+
+    $env:CNI_COMMAND="ADD"
+    $env:CNI_CONTAINERID="dummy"
+    $env:CNI_NETNS="dummy"
+    $env:CNI_IFNAME="dummy"
+    $env:CNI_PATH="c:\opt\cni\bin" #path to host-local.exe
+
+    cd $env:CNI_PATH
+    If(!(Test-Path c:\k\sourceVip.json)){
+        Get-Content c:\k\sourceVipRequest.json | .\win-overlay.exe | Out-File "c:\k\sourceVip.json"
+    }
+
+    Remove-Item env:CNI_COMMAND
+    Remove-Item env:CNI_CONTAINERID
+    Remove-Item env:CNI_NETNS
+    Remove-Item env:CNI_IFNAME
+    Remove-Item env:CNI_PATH
+
+}
+
+Export-ModuleMember -Function Get-SourceVip
