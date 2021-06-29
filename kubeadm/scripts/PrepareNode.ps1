c060c81c7d0ea561ce587e036f79581e309e7e21
jay vyas <jvyas@vmware.com>
2021-06-29 08:44:03 -0400

    Update PrepareNode.ps1

--- kubeadm/scripts/PrepareNode.ps1
+++ kubeadm/scripts/PrepareNode.ps1
@@ -1,4 +1,20 @@
 <#
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+#>
+
+<#
 .SYNOPSIS
 Assists with preparing a Windows VM prior to calling kubeadm join
 
@@ -20,21 +36,34 @@ PS> .\PrepareNode.ps1 -KubernetesVersion v1.19.3 -ContainerRuntime containerD
 #>
 
 Param(
-    [parameter(Mandatory = $true, HelpMessage="Kubernetes version to use")]
-    [string] $KubernetesVersion,
+    [parameter(HelpMessage="Kubernetes version to use", Mandatory = $false)]
+    [string] $KubernetesVersion = "1.21.0",
+
     [parameter(HelpMessage="Container runtime that Kubernets will use")]
     [ValidateSet("containerD", "Docker")]
-    [string] $ContainerRuntime = "Docker"
+    [string] $ContainerRuntime = "Docker",
+
+    # This is a modifcation for the windows-dev-tools where we
+    # OVERWRITE the WINDOWS kubelet AND kubeadm BINARY.
+    [parameter(HelpMessage="Allows to overwrite bins with self-built ones")]
+    [switch] $OverwriteBins
 )
 $ErrorActionPreference = 'Stop'
+Write-Output "Overwriting bins is set to '$OverwriteBins'"
 
 function DownloadFile($destination, $source) {
-    Write-Host("Downloading $source to $destination")
-    curl.exe --silent --fail -Lo $destination $source
+    if (Test-Path -Path $destination) {
+        Write-Host("Skipping download to avoid overwriting, already found on disk...")
+        return
+    }
+    else {
+        Write-Host("Downloading $source to $destination")
+        curl.exe --silent --fail -Lo $destination $source
 
-    if (!$?) {
-        Write-Error "Download $source failed"
-        exit 1
+        if (!$?) {
+            Write-Error "Download $source failed"
+            exit 1
+        }
     }
 }
 
@@ -65,9 +94,30 @@ mkdir -force "$global:KubernetesPath"
 $env:Path += ";$global:KubernetesPath"
 [Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
 
-DownloadFile $kubeletBinPath https://dl.k8s.io/$KubernetesVersion/bin/windows/amd64/kubelet.exe
+# DownloadFile $kubeletBinPath https://dl.k8s.io/$KubernetesVersion/bin/windows/amd64/kubelet.exe
+# We replaced this ↑ with ↓
+Write-Output "Deciding source to use for Kubelet.exe ..."
+$SelfBuiltKubeletSource = "C:\sync\windows\bin\kubelet.exe"
+if ((Test-Path -Path $SelfBuiltKubeletSource -PathType Leaf) -and ($OverwriteBins)) {
+    New-Item -ItemType File -Path $kubeletBinPath -Force
+    Write-Output "Found $SelfBuiltKubeletSource, copyin ..."
+    Copy-Item  $SelfBuiltKubeletSource -Destination $kubeletBinPath -Force
+} else {
+    Write-Output "Didn't find $SelfBuiltKubeletSource, downloading ..."
+    DownloadFile $kubeletBinPath https://dl.k8s.io/$KubernetesVersion/bin/windows/amd64/kubelet.exe
+}
+
+# Copying the self-built bins to windows
+# TODO does this even overwrite the kube-proxy.exe? where does kube-proxy.exe come from if we dont copy it here?
+$SelfBuiltKubeProxySource = "C:\sync\windows\bin\kube-proxy.exe"
+$KubeProxyPath = "C:\k\bin\kube-proxy.exe"
+if ($OverwriteBins) {
+    New-Item -ItemType File -Path $KubeProxyPath -Force
+    Write-Output "Copying $SelfBuiltKubeletPath"
+    Copy-Item  $SelfBuiltKubeProxySource  -Destination $KubeProxyPath -Force
+}
+
 DownloadFile "$global:KubernetesPath\kubeadm.exe" https://dl.k8s.io/$KubernetesVersion/bin/windows/amd64/kubeadm.exe
-DownloadFile "$global:KubernetesPath\wins.exe" https://github.com/rancher/wins/releases/download/v0.0.4/wins.exe
 
 if ($ContainerRuntime -eq "Docker") {
     # Create host network to allow kubelet to schedule hostNetwork pods
@@ -76,26 +126,29 @@ if ($ContainerRuntime -eq "Docker") {
     Write-Host "Creating Docker host network"
     docker network create -d nat host
 } elseif ($ContainerRuntime -eq "containerD") {
-    DownloadFile "c:\k\hns.psm1" https://github.com/Microsoft/SDN/raw/master/Kubernetes/windows/hns.psm1
-    Import-Module "c:\k\hns.psm1"
+    DownloadFile "C:\k\hns.psm1" https://github.com/Microsoft/SDN/raw/master/Kubernetes/windows/hns.psm1
+    Import-Module "C:\k\hns.psm1"
     # TODO(marosset): check if network already exists before creatation
     New-HnsNetwork -Type NAT -Name nat
 }
 
-Write-Host "Registering wins service"
-wins.exe srv app run --register
-start-service rancher-wins
+# Who needs wins were on the cusp of priveliged containers
+# Write-Host "Registering wins service"
+# wins.exe srv app run --register
+# start-service rancher-wins
 
 mkdir -force C:\var\log\kubelet
 mkdir -force C:\var\lib\kubelet\etc\kubernetes
 mkdir -force C:\etc\kubernetes\pki
-New-Item -path C:\var\lib\kubelet\etc\kubernetes\pki -type SymbolicLink -value C:\etc\kubernetes\pki\
+New-Item -force -path C:\var\lib\kubelet\etc\kubernetes\pki -type SymbolicLink -value C:\etc\kubernetes\pki\
 
 $StartKubeletFileContent = '$FileContent = Get-Content -Path "/var/lib/kubelet/kubeadm-flags.env"
 $global:KubeletArgs = $FileContent.TrimStart(''KUBELET_KUBEADM_ARGS='').Trim(''"'')
 
 $global:containerRuntime = {{CONTAINER_RUNTIME}}
 
+# TODO: This is useless because we only support containerd in this repo.
+# Lets delete at some point upstream and delete this also
 if ($global:containerRuntime -eq "Docker") {
     $netId = docker network ls -f name=host --format "{{ .ID }}"
 
@@ -109,6 +162,9 @@ $cmd = "C:\k\kubelet.exe $global:KubeletArgs --cert-dir=$env:SYSTEMDRIVE\var\lib
 Invoke-Expression $cmd'
 $StartKubeletFileContent = $StartKubeletFileContent -replace "{{CONTAINER_RUNTIME}}", "`"$ContainerRuntime`""
 Set-Content -Path $global:StartKubeletScript -Value $StartKubeletFileContent
+Write-Host "KUBEADM INITIAL FILE CONTENTS............"
+Write-Host $StartKubeletFileContent
+
 
 Write-Host "Installing nssm"
 $arch = "win32"
