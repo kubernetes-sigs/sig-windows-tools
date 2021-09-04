10e8dc7362a13211aa8722175f734a9e3a0b97b3
James Sturtevant <jstur@microsoft.com>
2021-09-03 17:59:42 -0700

    Add kube-proxy implementation for calico

--- hostprocess/calico/build.sh
+++ hostprocess/calico/build.sh
@@ -8,6 +8,9 @@ while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
   -n | --node )
     shift; node="1"
     ;;
+  -p | --proxy )
+    shift; proxy="1"
+    ;;
   -a | --all )
     shift; all="1"
     ;;
@@ -15,6 +18,8 @@ esac; shift; done
 if [[ "$1" == '--' ]]; then shift; fi
 
 docker buildx create --name img-builder --use --platform windows/amd64
+trap 'docker buildx rm img-builder' EXIT
+
 if [[ "$installer" == "1" || "$all" == "1" ]] ; then
     trap 'docker buildx rm img-builder' EXIT
     pushd install
@@ -28,4 +33,8 @@ if [[ "$node" == "1" || "$all" == "1" ]] ; then
     popd
 fi
 
-docker buildx rm img-builder || true
+if [[ "$proxy" == "1" || "$all" == "1" ]] ; then
+    pushd kube-proxy
+    docker buildx build --platform windows/amd64 --output=type=registry --pull -f Dockerfile -t jsturtevant/kube-proxy:calico-hostprocess .
+    popd
+fi
