d3cce15533d547171179421b74226cd18ed2e438
hxietkg <81352440+hxietkg@users.noreply.github.com>
2021-10-14 22:23:56 +0800

    Revise author in license (#176)
    Change author to 'The Kubnernetes Authoers' in license header

--- burrito/utils/config.go
+++ burrito/utils/config.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 Peri Thompson
+Copyright © 2021 The Kubnernetes Authoers
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
