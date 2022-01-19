29fa6d6009fca4ae2c85023861995e56bf97d024
Mark Rossetti <marosset@microsoft.com>
2022-01-19 09:08:04 -0800

    Updating csi-proxy build scripts to not push for ci builds

--- hostprocess/csi-proxy/build.sh
+++ hostprocess/csi-proxy/build.sh
@@ -1,28 +1,61 @@
 #!/bin/bash
-set -x
 set -e
 
+for i in "$@"; do
+  case $i in
+    --push)
+      push="1"
+      shift
+      ;;
+    *)
+    ;;
+  esac
+done
+
 if [[ -z "${version}" ]]; then
-    echo "Required env var 'version' is not set"
-    exit 1
+  echo "Required env var 'version' is not set"
+  exit 1
 fi
 echo "Using version ${version}"
 
+output="type=docker,dest=./export.tar"
+
+if [[ "$push" == "1" ]]; then
+  output="type=registry"
+fi
+
 repository=${repository:-"ghcr.io/kubernetes-sigs/sig-windows"}
+
+set -x
 
 docker buildx create --name img-builder --use --platform windows/amd64
 trap 'docker buildx rm img-builder' EXIT
 
-docker buildx build --platform windows/amd64 --output=type=registry -f Dockerfile.windows --build-arg=WINDOWS_VERSION=1809 -t ${repository}/csi-proxy:${version}-1809 .
-docker buildx build --platform windows/amd64 --output=type=registry -f Dockerfile.windows --build-arg=WINDOWS_VERSION=ltsc2022 -t ${repository}/csi-proxy:${version}-ltsc2022 .
 
-docker manifest create ${repository}/csi-proxy:${version} ${repository}/csi-proxy:${version}-1809 ${repository}/csi-proxy:${version}-ltsc2022
+declare -a win_vers=("1809" "ltsc2022")
 
-os_version_1809=$(docker manifest inspect mcr.microsoft.com/windows/nanoserver:1809 | grep "os.version" | head -n 1 | awk -F\" '{print $4}')
-docker manifest annotate --os windows --arch amd64 --os-version $os_version_1809 $repository/csi-proxy:$version $repository/csi-proxy:$version-1809
+manifest_entries=""
 
-os_version_ltsc2022=$(docker manifest inspect mcr.microsoft.com/windows/nanoserver:ltsc2022 | grep "os.version" | head -n 1 | awk -F\" '{print $4}')
-docker manifest annotate --os windows --arch amd64 --os-version $os_version_ltsc2022 $repository/csi-proxy:$version $repository/csi-proxy:$version-ltsc2022
+# Build container images with buildx
+for win_ver in "${win_vers[@]}"; do
+  docker buildx build --platform windows/amd64 --output=$output -f Dockerfile.windows --build-arg=WINDOWS_VERSION=$win_ver -t ${repository}/csi-proxy:${version}-$win_ver .
+
+  manifest_entries="$manifest_entries ${repository}/csi-proxy:${version}-$win_ver"
+done
+
+if [[ $push != "1" ]]; then
+  exit
+fi
+
+# Create manifest
+docker manifest create ${repository}/csi-proxy:${version} $manifest_entries
+
+# Annotate manifests
+for win_ver in "${win_vers[@]}"; do
+  os_ver=$(docker manifest inspect mcr.microsoft.com/windows/nanoserver:${win_ver} | grep "os.version" | head -n 1 | awk -F\" '{print $4}')
+  docker manifest annotate --os windows --arch amd64 --os-version $os_ver $repository/csi-proxy:$version $repository/csi-proxy:$version-$win_ver
+done
 
 docker manifest inspect $repository/csi-proxy:$version
+
 docker manifest push ${repository}/csi-proxy:${version}
