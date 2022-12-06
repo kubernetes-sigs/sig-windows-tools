37ca08816a22fa789099a85819b724ebfa62ef50
Mark Rossetti <marosset@microsoft.com>
2022-12-06 13:18:37 -0800

    removing nat network from containerd config and pointing to WIP PR for configuring CNI w/ (#254)
    containerd
    
    Signed-off-by: Mark Rossetti <marosset@microsoft.com>
    
    Signed-off-by: Mark Rossetti <marosset@microsoft.com>

--- kubeadm/scripts/Install-Containerd.ps1
+++ kubeadm/scripts/Install-Containerd.ps1
@@ -7,7 +7,6 @@ This script
 - Verifies that Windows Features requried for running contianers are enabled (and enables then if they are not)
 - Downloads ContainerD binaries from from at the version specified.
 - Downloads Windows SND CNI plugins.
-- Sets up a basic nat networking config for ContainerD to use until another CNI is configured
 - Registers ContainerD as a windows service.
 
 .PARAMETER ContainerDVersion
@@ -23,7 +22,7 @@ PS> .\Install-Conatinerd.ps1
 
 Param(
     [parameter(HelpMessage = "ContainerD version to use")]
-    [string] $ContainerDVersion = "1.4.1",
+    [string] $ContainerDVersion = "1.6.8",
     [parameter(HelpMessage = "Name of network adapter to use when configuring basic nat network")]
     [string] $netAdapterName = "Ethernet"
 )
@@ -38,48 +37,6 @@ function DownloadFile($destination, $source) {
         Write-Error "Download $source failed"
         exit 1
     }
-}
-
-<#
-.DESCRIPTION
-Computes a subnet for a gateway from the IPv4 IPAddress and PrefixLength properties
-for a given network adapter. This value is used for IPAM in a nat CNI config required for
-containerd.
-
-.NOTES 
-This logic is adapted from 
-https://github.com/containerd/containerd/blob/4a6b47d470d9f2dfc3d49f2819b968861dfa123e/script/setup/install-cni-windows
-
-.EXAMPLE
-PS> CalculateSubNet -gateway 172.16.5.8 -prefixLength 24
-172.16.5.0/8
-#>
-function CalculateSubNet {
-    param (
-        [string]$gateway,
-        [int]$prefixLength
-    )
-    $len = $prefixLength
-    $parts = $gateway.Split('.')
-    $result = @()
-    for ($i = 0; $i -le 3; $i++) {
-        if ($len -ge 8) {
-            $mask = 255
-
-        }
-        elseif ($len -gt 0) {
-            $mask = ((256 - 2 * (8 - $len)))
-        }
-        else {
-            $mask = 0
-        }
-        $len -= 8
-        $result += ([int]$parts[$i] -band $mask)
-    }
-
-    $subnetIp = [string]::Join('.', $result)
-    $cidr = 32 - $prefixLength
-    return "${subnetIp}/$cidr"
 }
 
 $requiredWindowsFeatures = @(
@@ -130,33 +87,6 @@ mkdir -Force c:\etc\cni\net.d | Out-Null
 Write-Output "Getting SDN CNI binaries"
 DownloadFile "c:\opt\cni\cni-plugins.zip" https://github.com/microsoft/windows-container-networking/releases/download/v0.2.0/windows-container-networking-cni-amd64-v0.2.0.zip
 Expand-Archive -Path "c:\opt\cni\cni-plugins.zip" -DestinationPath "c:\opt\cni\bin" -Force
-
-Write-Output "Creating network config for nat network"
-$gateway = (Get-NetIPAddress -InterfaceAlias $netAdapterName -AddressFamily IPv4).IPAddress
-$prefixLength = (Get-NetIPAddress -InterfaceAlias $netAdapterName -AddressFamily IPv4).PrefixLength
-
-$subnet = CalculateSubNet -gateway $gateway -prefixLength $prefixLength
-
-@"
-{
-    "cniVersion": "0.2.0",
-    "name": "nat",
-    "type": "nat",
-    "master": "Ethernet",
-    "ipam": {
-        "subnet": "$subnet",
-        "routes": [
-            {
-                "GW": "$gateway"
-            }
-        ]
-    },
-    "capabilities": {
-        "portMappings": true,
-        "dns": true
-    }
-}
-"@ | Set-Content "c:\etc\cni\net.d\0-containerd-nat.json" -Force
 
 Write-Output "Registering ContainerD as a service"
 containerd.exe --register-service
