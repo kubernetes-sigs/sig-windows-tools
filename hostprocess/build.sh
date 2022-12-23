90a275f5b1b27be6ed7dc59d6aab0c82c766b983
Fabian Fulga <ffulga@cloudbasesolutions.com>
2022-12-23 16:10:56 +0200

    Squashed commit of the following:
    commit de083fdeda9d4e938d8c5e69a3e45c4aa68a3d3c
    Merge: 328636a ba55f20
    Author: Fabian Fulga <ffulga@cloudbasesolutions.com>
    Date:   Fri Dec 23 15:57:40 2022 +0200
    
        Merge branch 'master' of https://github.com/kubernetes-sigs/sig-windows-tools into Updates-hostprocess-kube-proxy-build-script
    
    commit 328636a1ee718a1d4ad5fb802116268921ed48c7
    Author: Fabian Fulga <ffulga@cloudbasesolutions.com>
    Date:   Thu Jul 21 11:31:52 2022 +0300
    
        Update build.sh
    
    commit 7a9d42957f632a39c7e81679ddcc028002a136a9
    Author: Fabian Fulga <ffulga@cloudbasesolutions.com>
    Date:   Wed Jul 13 14:59:03 2022 +0300
    
        Update hostprocess kube-proxy build script
    
        Updated the hostprocess kube-proxy build script in order to be able to build all kube-proxy versions above v1.22.0 and made it possible to build a specific version if needed

--- hostprocess/build.sh
+++ hostprocess/build.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+<<'DESCRIPTION'
+  Script to build flannel and callico versions as well as their kube-proxy versions
+  There is also the posibility to build only a specific kube-proxy image or change the repository
+  and flannel/calico versions via the environment variables in the script,
+  in order to build it for other versions of them
+  
+  .EXAMPLE
+  ./build.sh --kubeProxyVersion v1.24.3
+  ./build.sh --kubeProxyVersion v1.24.3 -r sigwindowstools --flannelVersion v0.14.0
+  ./build.sh --kubeProxyVersion v1.24.3 -r sigwindowstools --calicoVersion v3.23.0
+
+DESCRIPTION
 
 repository=${repository:-"sigwindowstools"}
 flannelVersion=${flannelVersion:-"v0.14.0"}
-calicoVersion=${calicoVersion:-"v3.23.3"}
+calicoVersion=${calicoVersion:-"v3.23.0"}
+kubeProxyVersion=${kubeProxyVersion:-""}
+minVersion=${minVersion:-"v1.23.0"}
 
 SCRIPTROOT=$(dirname "${BASH_SOURCE[0]}")
 pushd $SCRIPTROOT/flannel
@@ -12,14 +26,24 @@ pushd $SCRIPTROOT/calico
 ./build.sh -r $repository --calicoVersion $calicoVersion
 popd
 
-declare -a proxyVersions=("v1.22.17" "v1.23.15" "v1.24.9" "v1.25.5" "v1.26.0")
+declare -a proxyVersions=($(curl -L registry.k8s.io/v2/kube-proxy/tags/list | jq .tags | tr "\n" " " | tr -d '"' | tr -d ','))
+
+if [ -n "$kubeProxyVersion" ]
+then
+  proxyVersions=("$kubeProxyVersion")
+fi
 
 # Read the array values with space
 for proxyVersion in "${proxyVersions[@]}"; do
-  pushd $SCRIPTROOT/flannel
-  ./build.sh -r $repository --proxyVersion $proxyVersion
-  popd
-  pushd $SCRIPTROOT/calico
-  ./build.sh -r $repository --proxyVersion $proxyVersion
-  popd
+  if [[ "$proxyVersion" =~ ^v([0-9]+\.[0-9]+\.[0-9]+)$ && $minVersion == `echo -e "$minVersion\n$proxyVersion" | sort -V | head -n1` ]]
+  then
+    echo "Building flannel proxy version $proxyVersion"
+    pushd $SCRIPTROOT/flannel
+    ./build.sh -r $repository --proxyVersion $proxyVersion
+    popd
+    echo "Building calico proxy version $proxyVersion"
+    pushd $SCRIPTROOT/calico
+    ./build.sh -r $repository --proxyVersion $proxyVersion
+    popd
+  fi
 done
