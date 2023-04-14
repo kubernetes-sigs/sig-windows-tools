ee59f5de23a54d53a0e21531aab5beedf2bd0058
James Sturtevant <jstur@microsoft.com>
2023-04-14 11:34:42 -0700

    Remove dockershim files (#271)

--- hack/DebugWindowsNode.ps1
+++ hack/DebugWindowsNode.ps1
@@ -28,46 +28,6 @@ Describe "Windows Version and Prerequisites" {
     }
 }
 
-Describe "Docker is installed" {
-    $services = Get-Service | Where-Object {($_.Name -eq "Docker") -or ($_.Name -eq "com.Docker.Service")}
-
-    It "A Docker service is installed - 'Docker' or 'com.Docker.Service' " {
-        $services| Should Not BeNullOrEmpty
-    }
-
-    It "Service is running" {
-        $AtLeastOneRunning = $false;
-        foreach ($service in $services)
-        {
-           #if there is more than 1 only one can be running
-           if ($service.Status -eq "Running")
-           {
-                $AtLeastOneRunning = $true
-           }
-        }
-        $AtLeastOneRunning | Should Be $true
-    }
-
-    It "Docker.exe is in path" {
-        # This also captures 'docker info' and 'docker version' output to be shown later
-        {
-            $versionText = & docker.exe version --format '{{ json . }}'
-            $global:DockerVersion = $versionText | ConvertFrom-Json
-        } | Should Not Throw
-    }
-
-    It "Should be a supported version" {
-        $supported = $false
-        if ($global:DockerVersion.Server.Version -match "\d+\.\d+\.\d+") {
-             $dockerVersion = [version]$Matches[0]
-             $supported = $dockerVersion -ge [version]"19.3.0"
-        }
-        $supported | Should Be $true
-    }
-}
-
-Write-Output "Docker version: $($global:DockerVersion.Server.Version)"
-
 Describe "Kubernetes processes are running" {
     It "There is 1 running kubelet.exe process" {
         (Get-Process "kubelet").Count | Should BeExactly 1
